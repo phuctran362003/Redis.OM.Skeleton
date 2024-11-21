8686f9e75eb7d784bd3e95293c38a9e5775957ca
Trần Gia Phúc <trangiaphuc362003181@gmail.com>
2024-11-21 07:46:41 +0700

    test Redis

---
+++ HostedServices/IndexCreationService .cs
@@ -0,0 +1,25 @@
+﻿using Redis.OM.Skeleton.Model;
+
+namespace Redis.OM.Skeleton.HostedServices
+{
+    public class IndexCreationService : IHostedService
+    {
+        private readonly RedisConnectionProvider _provider;
+        public IndexCreationService(RedisConnectionProvider provider)
+        {
+            _provider = provider;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            await _provider.Connection.CreateIndexAsync(typeof(Person));
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+
+    }
+}
